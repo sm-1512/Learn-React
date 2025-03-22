db23a3ff19d56b977da39d6bb6dc2ae27e4b2335
Sahil Mishra <sahil.mishra1512@gmail.com>
2025-03-22 16:20:50 +0530

    Password Generator Project

---
+++ 05passwordGenerator/src/App.jsx
@@ -0,0 +1,81 @@
+import { useState, useCallback, useEffect } from "react";
+
+function App() {
+  const [length, setLength] = useState(8);
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [characterAllowed, setCharactersAllowed] = useState(false);
+  const [Password, setPassword] = useState("");
+
+  const passwordGenerator = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if (numberAllowed) str += "0123456789";
+    if (characterAllowed) str += "!@#$%^&*{}[]()";
+
+    for (let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1); //Generating a random number
+      pass += str.charAt(char);
+    }
+    setPassword(pass);
+  }, [length, numberAllowed, characterAllowed, setPassword]); //Here we are providing setPassword as a dependency and not Password because then when the page reloads componet is rendered, useEffect triggered, and then inside it the Password which is the state is updated which will again cause useEffect to run as that state(Password) is a dependency. As a result there will be an infinite loop.
+  useEffect(() => {passwordGenerator()}, [length, numberAllowed, characterAllowed, passwordGenerator]) 
+  return (
+    <>
+      <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 text-orange-500 bg-gray-500">
+        <h1 className="text-white text-center my-3">Password Generator</h1>
+        <div className="flex shadow rounded-lg overflow-hidden mb-4">
+          <input
+            type="text"
+            value={Password}
+            className="outline-none w-full py-1 px-3"
+            placeholder="Password"
+            readOnly
+          />
+          <button className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0">
+            Copy
+          </button>
+        </div>
+
+        <div className="flex text-sm gap-x-2">
+          <div className="flex items-center gap-x-1">
+            <input
+              type="range"
+              min={8}
+              max={100}
+              value={length}
+              className="cursor-pointer"
+              onChange={(e) => {
+                setLength(e.target.value); //Here event 'e' is added as we need access to the event to change length
+              }}
+            />
+            <label>Length: {length}</label>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <input
+              type="checkbox"
+              defaultChecked={numberAllowed}
+              id="numberInput"
+              onChange={() => {
+                setNumberAllowed((prev) => !prev); //Here event not added as we do not not need access to the event. However if I add e to the callback there is no issue.
+              }}
+            />
+            <label htmlFor="numberInput">Numbers</label>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <input
+              type="checkbox"
+              id="characterAllowed"
+              defaultChecked={characterAllowed}
+              onChange={() => {
+                setCharactersAllowed((prev) => !prev); //In this case prev is passed as an event as we need to access that event to change it
+              }}
+            />
+            <label htmlFor="characterAllowed">Characters</label>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
