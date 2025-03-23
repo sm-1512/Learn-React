ed59555bdc64ff5d1fa4ead43e0d6ea1d55f288d
Sahil Mishra <sahil.mishra1512@gmail.com>
2025-03-23 22:37:43 +0530

    Currency Conversion Project

---
+++ 06currencyConverter/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+
+function useCurrencyInfo(currency) {
+    const [Data, setData] = useState({})
+  useEffect(() => {
+    fetch(
+      `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`
+    ).then((res) => res.json())  //Response is converted to JSON
+    .then((res) => setData(res[currency]))
+    console.log(Data);
+    
+  }, [currency]);
+  console.log(Data);
+  return Data
+  
+}
+
+export default useCurrencyInfo;
